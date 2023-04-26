7d8cadacbdd826634f800b9564ea70d26ea4413d
Bryan-Az <baambriz@ucsd.edu>
2023-04-26 19:02:29 +0000

    cleaned up LA scripts based on usecase features in notebooks

--- scripts/sql_scripts/latin_art_people.sql
+++ scripts/sql_scripts/latin_art_people.sql
@@ -1,32 +1,37 @@
+/* The output of this query is the first file containing relevent information about the artwork (physical and digital),
+    and the artist. (object constituents (link rels b/w art and artists) + constituents (artist info) + L.A. geo details (stats))
+    
+    P.S.
+    Some columns were not selected to avoid memory usage.
+    
+    constituents table may filter relationships more using constituentType column (the same artwork will be matched to groups of people if the 'individual' constituentType is not selected - leading to many row data for a single url/artwork).
+    
+    object_constituents table may filter relationships more than 'artistofNGAobject' column does in constituents using the roleType col. roleType filters based on types of artist (painter, publisher, author, etc.)
+    
+    Reduction possible by including an extra where key in the object_constituents select using these columns.
+    
+*/
 SELECT objectid,
 b.constituentid,
-displayorder,
 roletype,
 role,
-prefix,
-suffix,
-displaydate as displaydate_linkMade,
-beginyear as beginyear_linkMade,
-endyear as endyear_linkMade,
-b.country as country_linkMade,
-zipcode,
+displaydate as displaydate_artMade,
+beginyear as beginyear_artMade,
+endyear as endyear_artMade,
+b.country as country_artMade,
+zipcode as zipcode_artMade,
 ulanid,
-preferreddisplayname,
 forwarddisplayname,
-lastname,
-displaydate,
 artistofngaobject,
 beginyear,
 endyear, 
-visualbrowsertimespan,
 nationality,
-visualbrowsernationality,
 constituenttype,
-Demonym,
+Demonym as demonym,
 pct_country_NGA,
-countryCode,
-Continent,
-`Country Name`
+countryCode as artists_countryCode,
+Continent as artists_continent,
+`Country Name` as artists_country
 FROM objects_constituents as b
 INNER JOIN (
     SELECT *
--- scripts/sql_scripts/latin_art_urls.sql
+++ scripts/sql_scripts/latin_art_urls.sql
@@ -1,4 +1,37 @@
-SELECT *
+/*
+   The output of this query is the second file containing all information in latin_art_people,      and adds more columns like urls and digital info in published_images. 
+*/
+SELECT uuid,
+iiifurl,
+iiifthumburl,
+viewtype,
+sequence,
+width,
+height,
+maxpixels,
+assistivetext,
+depictstmsobjectid,
+objectid,
+constituentid,
+roletype,
+`role`,
+displaydate_linkMade,
+beginyear_artMade,
+endyear_artMade,
+country_artMade,
+zipcode_artMade,
+ulanid,
+forwarddisplayname,
+artistofngaobject,
+beginyear,
+endyear,
+nationality,
+constituenttype,
+demonym,
+pct_country_NGA,
+artists_countryCode,
+artists_continent,
+artists_country
 FROM published_images
 INNER JOIN latin_art_people
 ON published_images.depictstmsobjectid = latin_art_people.objectid
