50a0603cf8f2d6a7e3ecbc54892cbc4c0429603d
Bryan-Az <baambriz@ucsd.edu>
2023-04-26 06:03:05 +0000

    modified notebooks to transer into script and updated csv files to match updated sql scripts

---
+++ scripts/rename_LaArt.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+import re
+#-- This File is Step 2 of the LaArt Pipeline --
+#import the metadata for La Art (this is a result of a MySql Database query script)
+la_image_metadata = pd.read_csv('../data_samples/results/whole_set_results/pa_latin_art.csv')
+#dropping unneccessary columns
+la_image_metadata = la_image_metadata.drop(['accessioned', 'displaydate'], axis = 1)
+
+
+#creating country demographic statistics
+proportion_ofCountry = la_image_metadata['Country Name'].value_counts(normalize=True)
+la_image_metadata['percent_fromCountry'] = la_image_metadata['Country Name'].apply(lambda x: proportion_ofCountry[x])
+la_image_metadata = la_image_metadata.rename({'Percent_in_NGA': 'percent_fromArtist'}, axis=1)
+
+
+#setting path to send images during download (in step 3: download_LaArt)
+la_image_directory = '../latinamerican-2-imagefolder-split/'
+la_image_metadata['directory'] = [la_image_directory] * len(la_image_metadata)
+
+
+
+## for next code section in notebook, it is dependent on images being downloaded
+## since rename notebook was created during testing, and this is a production script, 
+## we assume no images are downloaded and image_fp in pa_latin_art is not set yet
--- scripts/sql_scripts/latin_art_people.sql
+++ scripts/sql_scripts/latin_art_people.sql
@@ -1,10 +1,38 @@
-SELECT *
-FROM objects_constituents
+SELECT objectid,
+b.constituentid,
+displayorder,
+roletype,
+role,
+prefix,
+suffix,
+displaydate as displaydate_linkMade,
+beginyear as beginyear_linkMade,
+endyear as endyear_linkMade,
+b.country as country_linkMade,
+zipcode,
+ulanid,
+preferreddisplayname,
+forwarddisplayname,
+lastname,
+displaydate,
+artistofngaobject,
+beginyear,
+endyear, 
+visualbrowsertimespan,
+nationality,
+visualbrowsernationality,
+constituenttype,
+Demonym,
+pct_country_NGA,
+countryCode,
+Continent,
+`Country Name`
+FROM objects_constituents as b
 INNER JOIN (
     SELECT *
     FROM (SELECT *
           FROM constituents
           WHERE constituents.artistofngaobject = 1) a
     INNER JOIN la_geographicStatistics
-    ON a.nationality = la_geographicStatistics."Demonym") x
-ON objects_constituents.constituentid = x.constituentid
+    ON a.nationality = la_geographicStatistics.Demonym) x
+ON b.constituentid = x.constituentid
