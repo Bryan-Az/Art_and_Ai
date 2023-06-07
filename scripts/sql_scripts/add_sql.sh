d6a0287160b5f1c34b758b4c05148d63e8a10a1b
Bryan-Az <baambriz@ucsd.edu>
2023-06-07 08:19:53 +0000

    schema create merged with insert scripts + sql for nonLaData

--- scripts/sql_scripts/add_sql.sh
+++ scripts/sql_scripts/add_sql.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# run this after cding into sql_scripts folder
 #the SQL scripts in the 'create_schema' and 'insert_data' zips were created running MySQL>=8.0 DB
 #the "la_art_tables" zip will be placed into the data_samples/LaArt directory to be used by the add_LaArt.sh script since it has CSV and is a data sample
 
@@ -7,6 +8,5 @@ unzip ../../data_samples/LaArt/la_art_tables.zip -d ../../data_samples/LaArt/
 
 #these are used in the db
 #assumes zip package installed on ubuntu
-unzip ./create_schema.zip
 unzip ./insert_data.zip
 unzip ./query_tables.zip
