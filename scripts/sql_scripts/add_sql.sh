f052efdf1d83edeac390221f7be0e23ab244a193
Bryan-Az <baambriz@ucsd.edu>
2023-07-02 01:28:45 +0000

    imported csv zip of nonLaData, change in directory of data_samples

--- scripts/sql_scripts/add_sql.sh
+++ scripts/sql_scripts/add_sql.sh
@@ -4,8 +4,12 @@
 #the "la_art_tables" zip will be placed into the data_samples/LaArt directory to be used by the add_LaArt.sh script since it has CSV and is a data sample
 
 cd scripts/sql_scripts
-mv la_art_tables.zip ../../data_samples/LaArt
-unzip ../../data_samples/LaArt/la_art_tables.zip -d ../../data_samples/LaArt/
+mv art_tables.zip ../../data_samples/
+unzip ../../data_samples/art_tables.zip -d ../../data_samples/
+# within this folder there is 2 tables (1 for LA art/1 for nonLA)
+# these need to be moved into their respective subfolders in the parent directory
+
+
 
 #these are used in the db
 #assumes zip package installed on ubuntu
