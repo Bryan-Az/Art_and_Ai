6336c3d6330e71c4430f5248e3202f757ce404e7
Bryan-Az <baambriz@ucsd.edu>
2023-07-17 02:22:24 +0000

    FE of geographical data

--- scripts/pytorch_pipelines/add_nonLaArt/extract_nonLaArt.py
+++ scripts/pytorch_pipelines/add_nonLaArt/extract_nonLaArt.py
@@ -8,5 +8,7 @@ import pycountry
 import pycountry_convert as pc
 import random
 #this should be taken as non_latin_art.csv from scripts/source-sql
-nonLa_art = pd.read_csv('../../data_samples/results/whole_set_results/pa_nonLa_art.csv')
-la_geographicStatistics = pd.read_csv('../../data_samples/la_geographicStatistics.csv')
+#new version expects dtype on import however as of now only 1 row is impacted
+nonLa_art = pd.read_csv('../../../data_samples/art_tables/non_latin_art.csv', on_bad_lines='skip')
+# Missing from non latin art extract (but included in the latinart extract)
+#la_geographicStatistics = pd.read_csv('../../data_samples/la_geographicStatistics.csv')
